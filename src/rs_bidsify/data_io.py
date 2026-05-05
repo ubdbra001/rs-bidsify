896070c2779062ba2987852c738e67777c436d78
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-05-05 13:35:02 +0100

    Add logging to existing data_io read functions

--- src/rs_bidsify/data_io.py
+++ src/rs_bidsify/data_io.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import logging
 from pathlib import Path
 from typing import Any
 from rs_bidsify.utils import get_utc_today
@@ -9,7 +9,7 @@ from rs_bidsify.utils import get_utc_today
 import pandas as pd
 from mne.io import read_raw, BaseRaw
 
-from mne_bids import BIDSPath, print_dir_tree, write_raw_bids, make_dataset_description, update_sidecar_json
+logger = logging.getLogger(__name__)
 
 def read_description_json(file_path: Path) -> DatasetSpec:
     """Reads and validates the description json file.
@@ -27,9 +27,11 @@ def read_description_json(file_path: Path) -> DatasetSpec:
     raw_description = Path(file_path).read_text()
     validated_model = DatasetSpec.model_validate_json(raw_description)
 
+    logger.info(f"Loaded and validated description JSON: {file_path}")
+
     return validated_model
 
-def read_description_spreadsheet(sheet_path: Path, sheet_info: dict[str, Any]) -> dict[str, pd.DataFrame]:
+def read_description_spreadsheet(sheet_path: Path, sheet_info: dict[str, Any], sheet_type: str) -> dict[str, pd.DataFrame]:
     """Read sheets from the description spreadsheet
 
     Parameters
@@ -44,9 +46,12 @@ def read_description_spreadsheet(sheet_path: Path, sheet_info: dict[str, Any]) -
     dict[str, pd.DataFrame]
         Dictionary containing the loaded sheet(s), will typically consist of a datasheet and a codebook for the datasheet
     """    
-    """"""
+    
+    sheet_dict = {key: pd.read_excel(sheet_path, **val) for key, val in sheet_info.items()}
+    
+    logger.info(f"Loaded {sheet_type} info from {sheet_path}")
 
-    return {key: pd.read_excel(sheet_path, **val) for key, val in sheet_info.items()}
+    return sheet_dict
 
 
 def read_eeg_recording(recording_path: Path) -> BaseRaw:
