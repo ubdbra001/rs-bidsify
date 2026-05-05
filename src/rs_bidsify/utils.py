46a201b618fada1d4393348fd9f29c0ab19df902
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-05-05 12:04:03 +0100

    add find_file util

--- src/rs_bidsify/utils.py
+++ src/rs_bidsify/utils.py
@@ -1,4 +1,14 @@
 from datetime import datetime, timezone
+from pathlib import Path
 
 def get_utc_today() -> datetime:
     return datetime.now(timezone.utc)
+
+
+def find_file(path: Path, ext: str):
+    found_path = list(path.glob(f"*.{ext}"))
+
+    if len(found_path) != 1:
+        raise ValueError(f"Expected single {ext} file in {path}, instead found {len(found_path)}")
+    
+    return found_path[0]
