257379497a713f5b58adcfe07e9766daea3393b9
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-05-05 13:38:03 +0100

    Create high-level process_dataset function

--- src/rs_bidsify/processing.py
+++ src/rs_bidsify/processing.py
@@ -3,7 +3,16 @@ from pathlib import Path
 from rs_bidsify import data_io
 from rs_bidsify.config import PARTICIPANT_INFO as part_info, PHENOTYPE_INFO as phen_info
 from rs_bidsify.validation.description import DatasetDescription
+from rs_bidsify.validation.dataset import EEGDatasetCrawler, RecordingMetadata
 from rs_bidsify.utils import find_file
+def process_dataset(raw_path: Path, out_root_path: Path):
+
+    dataset_desc = generate_dataset_description(raw_path)
+
+    crawler = EEGDatasetCrawler(
+        root_path=raw_path,
+        **dataset_desc.crawler_info
+    )
 
 def generate_dataset_description(root_path: Path):
 
@@ -11,8 +20,8 @@ def generate_dataset_description(root_path: Path):
     sheet_path = find_file(root_path, "ods")
 
     dataset_spec = data_io.read_description_json(json_path)
-    participant_data = data_io.read_description_spreadsheet(sheet_path, part_info)
-    phenotype_data = data_io.read_description_spreadsheet(sheet_path, phen_info)
+    participant_data = data_io.read_description_spreadsheet(sheet_path, part_info, "participant")
+    phenotype_data = data_io.read_description_spreadsheet(sheet_path, phen_info, "phenotype")
 
     return DatasetDescription(spec=dataset_spec, participants=participant_data, phenotype=phenotype_data)
 
