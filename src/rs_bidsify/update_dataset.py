8bdc9579f2584e2969879a96e6a381ffad008f12
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-05-05 13:41:37 +0100

    Add functions for getting and setting subject info

---
+++ src/rs_bidsify/update_dataset.py
@@ -0,0 +1,31 @@
+import logging
+from mne.io import BaseRaw
+from pandas import DataFrame
+from rs_bidsify.validation.dataset import RecordingMetadata
+from rs_bidsify.validation.subject import SubjectMetadata
+
+logger = logging.getLogger(__name__)
+
+
+def get_subject_info(recording: RecordingMetadata, participants_df: DataFrame) -> SubjectMetadata:
+
+    part_id = recording.participant
+    subject_info = participants_df.loc[part_id].to_dict()
+
+    return SubjectMetadata(**subject_info) # type: ignore
+
+
+def set_subject_info(eeg_data: BaseRaw, subject_model: SubjectMetadata):
+    """Set the information about the subject"""
+
+    if eeg_data.info["subject_info"] is None:
+        eeg_data.info["subject_info"] = subject_model.model_dump()
+    elif isinstance(eeg_data.info["subject_info"], dict):
+        eeg_data.info["subject_info"].update(subject_model.model_dump())
+    else:
+        # may want to raise a warning here if there is something in
+        # subject_info, but it is not of an expected type
+        pass
+    
+    logger.info(f"Updated subject information: {subject_model}")
+
