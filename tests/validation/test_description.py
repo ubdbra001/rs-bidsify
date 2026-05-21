b426564bac18bdd87616f6eb557a29c63dc24547
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-05-21 23:31:22 +0100

    use pyfakefs over tmp_path_factory

--- tests/validation/test_description.py
+++ tests/validation/test_description.py
@@ -1,17 +1,18 @@
 import pytest
+from pathlib import Path
 from pydantic import ValidationError
 
 from rs_bidsify.validation import description as rs_desc
 
 
-class TestMontageInfo:
-    @pytest.fixture(scope="class")
-    def shared_file(self, tmp_path_factory):
-        temp_dir = tmp_path_factory.mktemp("data")
-        f = temp_dir / "test_file.loc"
-        f.write_text("dummy content")
-        return f
+@pytest.fixture
+def shared_file(fs) -> Path:
+    file_path = Path("data") / "test_file.loc"
+    fs.create_file(file_path, contents="dummy content")
+    return file_path
 
+
+class TestMontageInfo:
     def test_valid_name(self):
         data = {"mne_name": "standard_1020"}
         model = rs_desc.Montage.model_validate(data)
