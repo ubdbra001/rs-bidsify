db9b9796d59360e75129fcde9998c70479af70de
Dan Brady <daniel.brady@sheffield.ac.uk>
2026-06-01 14:33:41 +0100

    update noxfile Only run specific sessions and add an optional fix_lint session

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ from nox import Session, options
 from nox_uv import session
 
 options.default_venv_backend = "uv"
+options.sessions = ["lint", "type_check", "test"]
 
 @session(
     python=["3.12", "3.13", "3.14"],
@@ -18,3 +19,9 @@ def type_check(s: Session) -> None:
 def lint(s: Session) -> None:
     s.run("ruff", "check", ".")
     s.run("ruff", "format", "--check", ".")
+
+@session(uv_only_groups=["lint"])
+def fix_lint(s: Session) -> None:
+    s.run("ruff", "check", "--fix", ".")
+    s.run("ruff", "format", ".")
+
